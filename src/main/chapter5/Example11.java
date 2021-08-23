edbaafc690cea0ac6a763a3c0e7737a50f9307fe
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2021-08-23 22:18:53 +0100

    Jessica|Exceptions handling|Call throw to handle RunTimeException

---
+++ src/main/chapter5/Example11.java
@@ -0,0 +1,15 @@
+package main.chapter5;
+/*using throw to catch a RunTimeException*/
+
+public class Example11 {
+    public static void main(String[] args){
+        String text = null;
+
+        try {
+            System.out.println(text.length());
+        } catch (Exception e){
+            System.out.println("Exception; this should be a NullPointerException");
+            throw new RuntimeException();
+        }
+    }
+}
