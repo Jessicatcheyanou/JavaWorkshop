7c6c5c3c150828a0756c916949a8f68f5600e665
Jessicatcheyanou <jessicatcheyanou@gmail.com>
2021-12-16 02:08:04 +0100

    Jessica|Reflection|Extracting a list of Constructors

---
+++ src/main/chapter19/ConstructorInfo.java
@@ -0,0 +1,25 @@
+package main.chapter19;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.List;
+
+public class ConstructorInfo {
+
+    public static void extractConstructors(Class c){
+
+        List<Constructor> constructors = Arrays.asList(c.getDeclaredConstructors());
+        System.out.println(c.getSimpleName());
+        for (Constructor con : constructors){
+            System.out.println(" " + con.toGenericString());
+        }
+    }
+
+    public static void main(String[] args) {
+        extractConstructors(ClassInfo.class);
+        extractConstructors(SiberianTiger.class);
+        extractConstructors(Pets.class);
+        extractConstructors(Animal.class);
+        extractConstructors(java.lang.String.class);
+    }
+}
